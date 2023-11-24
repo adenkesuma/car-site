cfe5ef9ffcba43d8cadeaec4dfb01722c1758064
adenkesuma <adenfdfd10@gmail.com>
2023-11-24 12:01:48 +0700

    `feat:` add navbar

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+
+    </footer>
+  )
+}
+
+export default Footer
--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,11 @@
 import Hero from "./Hero"
 import CustomButton from "./CustomButton"
+import Footer from "./Footer"
+import Navbar from "./Navbar"
 
 export {
   Hero,
-  CustomButton
+  CustomButton,
+  Navbar,
+  Footer
 }
