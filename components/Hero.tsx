d10d1eb080b1c06a03ada8d622cddce22987c76d
adenkesuma <adenfdfd10@gmail.com>
2023-11-25 20:52:03 +0700

    `feat:` add search bar and search manufacturer component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,8 +32,8 @@ const Hero = () => {
             fill 
             className="object-contain"
           />
-          <div className="hero__image-overlay" />
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   )
